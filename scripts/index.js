72dbbf9fb7f1d133354ee8ec55da5342ed03386f
r2d2 <mrskvortsov@yandex.ru>
2024-03-12 09:47:00 +0300

    сдал на проверку

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-// @todo: Темплейт карточки
+const createCard = (data, deleteCallback) => {
+  const cardTemplate = document.getElementById('card-template');
+  const cardClone = cardTemplate.content.cloneNode(true);
 
-// @todo: DOM узлы
+  const cardImage = cardClone.querySelector('.card__image');
+  const cardTitle = cardClone.querySelector('.card__title');
+  const deleteButton = cardClone.querySelector('.card__delete-button');
 
-// @todo: Функция создания карточки
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
 
-// @todo: Функция удаления карточки
+  deleteButton.addEventListener('click', deleteCallback);
+  return cardClone;
+};
 
-// @todo: Вывести карточки на страницу
+const deleteCard = (event) => {
+  event.target.closest('.places__item').remove();
+};
+
+const cardContainer = document.querySelector('.places__list');
+
+initialCards.forEach(cardData => {
+  const cardElement = createCard(cardData, deleteCard);
+  cardContainer.appendChild(cardElement);
+});
